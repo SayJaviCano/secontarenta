580d8d3a8ee82900eadd673607e5c814616247cb
SayJaviCano <jc@saysawa.com>
2022-02-11 21:15:04 +0100

    Registro depurado

--- cypress/integration/tests/registro.spec.js
+++ cypress/integration/tests/registro.spec.js
@@ -1,8 +1,3 @@
-/// <reference types="Cypress" /> 
-
-require('cypress-xpath') // Cuando se utilice el xpath como selector
-require('cypress-plugin-tab') // Cuando se utilice la función .tab para saltar de campo
-
 
 describe("Registro", () => {  // Nombre de la Test Suite
 
